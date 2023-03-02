6aec5501440ddef17ba6cee537fe7f7692be279e
schmatteo <michal.skrzyszowski@hotmail.com>
2023-03-02 17:05:50 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var text = await File.ReadAllBytesAsync(args[0] ?? throw new ArgumentNullException(nameof(args), "No file specified"));
+
+        var outputPath = "./output.txt";
+        try
+        {
+            outputPath = args[1];
+        }
+        catch (IndexOutOfRangeException){}
+
+        await using StreamWriter sw = new(outputPath);
+        foreach (var b in text)
+        {
+            await sw.WriteLineAsync(b.ToString());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# read-bytes-from-file
+
+This program is made for reading files and saving them as strings of bytes.
+I don't really know what the purpose or real use case is. 
+I personally made it to see whether a file I tried reading included BOM.
+
+[Example output](https://pastebin.com/me1ki6GX)
+
+## Usage
+
+1. Extract the zip
+2. Open the terminal in the extracted directory
+3. Run it using `ReadByteStrings.exe <path-to-input-file> [<path-to-output-file>]`
